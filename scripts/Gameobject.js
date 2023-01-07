3490fabf50dbbbbb37fead24cf9bb4985ebfeac8
CHOO XIANG LUN <xlchoo25@gmail.com>
2023-01-07 20:46:47 +0800

    fix missing main character and remove useless images

--- scripts/Gameobject.js
+++ scripts/Gameobject.js
@@ -7,7 +7,7 @@ class GameObject {
     this.direction = config.direction || "down";
     this.sprite = new Sprite({
       gameObject: this,
-      src: config.src || "/assets/chara.png",
+      src: config.src || "assets/chara.png",
     });
 
     this.behaviorLoop = config.behaviorLoop || [];
